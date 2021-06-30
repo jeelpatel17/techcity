b6e7cf05a61240687200c8ec1304b2e9373d09d2
Jeel Patel <jeelsardar77@gmail.com>
2021-06-30 18:04:15 +0530

    added custom 404 page

--- components/header.js
+++ components/header.js
@@ -20,6 +20,10 @@ export default function Header(props) {
           href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <div className={styles.header}>
         <Link href="/">
--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,58 @@
+import Link from "next/link";
+import Head from "next/head";
+import styles from "../styles/404.module.css";
+// for cursor
+import React from "react";
+import dynamic from "next/dynamic";
+
 export default function Custom404() {
-  return <h1>404- My Custom 404 page!!!</h1>;
+  const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+    ssr: false,
+  });
+  return (
+    <>
+      <Head>
+        <title>Uh oh! Page not found!</title>
+      </Head>
+      {/* for cursor */}
+      <AnimatedCursor
+        color="65,105,225"
+        innerSize={12}
+        outerSize={24}
+        outerAlpha={0.2}
+        innerScale={0.4}
+        outerScale={4}
+      />
+      <body className={styles.body}>
+        <section className={styles.notFound}>
+          <div className={styles.img}>
+            <img
+              className={styles.image}
+              src="https://assets.codepen.io/5647096/backToTheHomepage.png"
+              alt="Back to the Homepage"
+            />
+            <img
+              className={styles.image}
+              src="https://assets.codepen.io/5647096/Delorean.png"
+              alt="El Delorean, El Doc y Marti McFly"
+            />
+          </div>
+          <div className={styles.text}>
+            <h1 className={styles.h1}>404</h1>
+            <h2 className={styles.h2}>PAGE NOT FOUND</h2>
+            <h3 className={styles.h3}>BACK TO HOME?</h3>
+            <Link href="/">
+              <a className={styles.a}>YES</a>
+            </Link>
+            <a
+              href="https://www.youtube.com/watch?v=G3AfIvJBcGo"
+              className={styles.a}
+            >
+              NO
+            </a>
+          </div>
+        </section>
+      </body>
+    </>
+  );
 }
